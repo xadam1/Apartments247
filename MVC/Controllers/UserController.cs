b5bc6bb109282bd14b5a86c9e4dde8868a08bedb
xadam1 <honza_a@volny.cz>
2021-01-09 15:40:05 +0100

    front end registration page

---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(UserNameEmailPassDTO userDto)
+        {
+
+
+            return new EmptyResult();
+        }
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MVC
 {
@@ -32,6 +27,7 @@ namespace MVC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
