78cb6162849ad5e1a763b43ce28eb2263cb58bbe
Vojtěch Škaroupka <485232@muni.cz>
2021-01-10 18:06:12 +0100

    Added missing attribute

--- WebAPI/Models/UnitWithSpecificationModel.cs
+++ WebAPI/Models/UnitWithSpecificationModel.cs
@@ -13,6 +13,7 @@ namespace WebAPI.Models
         public string Color { get; set; }
         public int AddressId { get; set; }
         public string Note { get; set; }
+        public int UnitTypeId { get; set; }
         public string UnitType { get; set; }
         public int CurrentCapacity { get; set; }
         public int MaxCapacity { get; set; }
