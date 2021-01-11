c887513c04af8c83c92e448ad24bfa137691a058
Vojtěch Škaroupka <485232@muni.cz>
2021-01-11 22:55:02 +0100

    Privacy polici view added

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using Newtonsoft.Json;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(int userId = -1, int groupId = -1)
         {
-            int userId = 1; // TODO
-            int groupId = Utils.GetFirstUnitGroupIdByUserId(userId);
+            if (userId == -1)
+            {
+                userId = 1; // TODO
+            }
+            if (groupId == -1)
+            {
+                groupId = Utils.GetFirstUnitGroupIdByUserId(userId);
+            }
 
             return RedirectToAction("Overview", "Overview", new { userId = userId, groupId = groupId });
+        }
+
+        [HttpGet]
+        public IActionResult Privacy(int userId, int groupId)
+        {
+            HomeModel m = new HomeModel()
+            {
+                UserId = userId,
+                GroupId = groupId,
+            };
+            return View(m);
         }
     }
 }
