183fd0c57510a621800ab4e684e31d2cbcd43d8e
xadam1 <honza_a@volny.cz>
2021-01-12 17:52:59 +0100

    Working_from_front-end

--- WebAppMVC/Utils/ApiConnectionUrls.cs
+++ WebAppMVC/Utils/ApiConnectionUrls.cs
@@ -2,7 +2,7 @@
 {
     public static class ApiConnectionUrls
     {
-        private static string local = @"https://localhost:44306/api/Api/";
+        private static string local = @"https://localhost:5000/api/Api/";
         private static string shared = @"http://cassiopeia.serveirc.com:5000/api/Api/";
 
         public static string API_URL { get; } = shared;
