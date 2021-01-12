5dd8e080d042af0aab936d160cfb9a779f768f35
IFofone <tomasfojt@seznam.cz>
2021-01-12 14:07:26 +0100

    added missing automapping for unitGroup and unitGroupDTO

--- BLL/BusinessMappingConfig.cs
+++ BLL/BusinessMappingConfig.cs
@@ -25,6 +25,7 @@ namespace BLL
             config.CreateMap<Unit, UnitDTO>().ReverseMap();
 
             config.CreateMap<UnitGroup, UnitGroupSpecificationUnitsDTO>().ReverseMap();
+            config.CreateMap<UnitGroup, UnitGroupDTO>().ReverseMap();
 
             config.CreateMap<UnitType, UnitTypeDTO>().ReverseMap();
 
