e1192f2a4d4bf5bc175785698d929970cb602566
xadam1 <honza_a@volny.cz>
2021-02-09 00:46:31 +0100

    New Project

---
+++ WebMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebMVC.Areas.Identity.Data;
+using WebMVC.Data;
+
+[assembly: HostingStartup(typeof(WebMVC.Areas.Identity.IdentityHostingStartup))]
+namespace WebMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<A247AuthContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("A247AuthContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<A247AuthContext>();
+            });
+        }
+    }
+}
