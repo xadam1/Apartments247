8622907c71598c45a2753fa039d6d6bad2661c0b
IFofone <tomasfojt@seznam.cz>
2021-02-13 13:53:19 +0100

    application handles invalid url

--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -57,6 +57,15 @@ namespace WebMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Home/Error";
+                    await next();
+                }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
